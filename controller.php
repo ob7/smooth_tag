edc31779f00dca4d7494459f8b56e0f5391387f3
OB7 Dev <dp@ob7.us>
2016-08-23 23:23:05 -0800

    Added single page and new block icon

--- controller.php
+++ controller.php
@@ -1,49 +1,57 @@
-<?php         
+<?php
 
 namespace Concrete\Package\SmoothTag;
 use Package;
+use SinglePage;
+use Page;
 
 defined('C5_EXECUTE') or die(_("Access Denied."));
 
 class Controller extends Package
 {
 
-	protected $pkgHandle = 'smooth_tag';
-	protected $appVersionRequired = '5.7.1';
-	protected $pkgVersion = '0.1.0';
-	
-	
-	
-	public function getPackageDescription()
-	{
-		return t("Adds smooth scrolling to a href tag links (such as #top)");
-	}
+    protected $pkgHandle = 'smooth_tag';
+    protected $appVersionRequired = '5.7.1';
+    protected $pkgVersion = '0.1.1';
 
-	public function getPackageName()
-	{
-		return t("Smooth A Tag Link Scrolling");
-	}
-	
-	public function install()
-	{
-		$pkg = parent::install();
-	}
-
-	public function on_start() 
-	{
-      \Events::addListener(
-        'on_page_view',
-        function ($e) {
-          $html = \Loader::helper('html');
-          $page = $e->getPageObject();
-
-          $systemPage = $page->isAdminArea();
-          if(!$systemPage) {
-            $v = \View::getInstance();
-            $v->addFooterItem($html->javascript('smoothTag.js', $this->pkgHandle));
-          }
-        }
-      );
+    public function getPackageDescription()
+    {
+        return t("Adds smooth scrolling to a href tag links (such as #top)");
     }
+
+    public function getPackageName()
+    {
+        return t("Scroll Tag");
+    }
+
+    public function install()
+    {
+        $pkg = parent::install();
+
+        $sp = Page::getByPath('/dashboard/scroll_tag');
+        if(!is_object($sp) || $sp->isError()) {
+            $sp = SinglePage::add('/dashboard/scroll_tag', $pkg);
+            $sp->update(array('cName'=>t('Scroll Tag Settings'), 'cDescription'=>'')); // this is displayed in the package overview after being installed, not on the actual dashboard pages
+        }
+
+    }
+
+    public function on_start() // code runs
+    {
+        \Events::addListener(
+            'on_page_view',
+            function ($e) {
+                $html = \Loader::helper('html');
+                $page = $e->getPageObject();
+
+                $systemPage = $page->isAdminArea(); //we dont want this enabled on admin pages
+                if(!$systemPage) {
+                    $v = \View::getInstance();
+                    $v->addFooterItem($html->javascript('smoothTag.js', $this->pkgHandle));
+                }
+            }
+        );
+    }
+
 }
 ?>
