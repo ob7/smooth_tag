ab93c8636ed159d9489c86c56a7a0a8fac907206
OB7 Dev <dp@ob7.us>
2016-08-24 17:50:29 -0800

    Added config data for exclude and include fields

--- controllers/single_page/dashboard/smooth_tag/settings.php
+++ controllers/single_page/dashboard/smooth_tag/settings.php
@@ -10,12 +10,23 @@ class Settings extends DashboardPageController {
     public function view()
     {
         $pkg = Package::getByHandle('smooth_tag');
+
+        // Get enable/disable config
         $enableSmoothTag = $pkg->getConfig()->get('archebian.smooth_tag.enabled');
         $this->set('enableSmoothTag', $enableSmoothTag);
+
+        // Get exluded selectors
+        $excludeSelectors = $pkg->getConfig()->get('archebian.smooth_tag.exclude');
+        $this->set('exclude', $excludeSelectors);
+
+        //Get included selecors
+        $includeSelectors = $pkg->getConfig()->get('archebian.smooth_tag.include');
+        $this->set('include', $includeSelectors);
 
         // Assets for bootstrap checkbox as switch
         $this->requireAsset('javascript','bootstrapswitch');
         $this->requireAsset('css','bootstrapswitch');
+        // bootstrap callouts css
         $this->requireAsset('css','bootstrapcallout');
     }
 
@@ -30,8 +41,12 @@ class Settings extends DashboardPageController {
         if ($this->token->validate("save_settings")) {
             if ($this->isPost()) {
                 $enableSmoothTag = $this->post('enableSmoothTag') ? 1 : 0;
+                $excludeSelectors = $this->post('exclude');
+                $includeSelectors = $this->post('include');
                 $pkg = Package::getByHandle('smooth_tag');
                 $pkg->getConfig()->save('archebian.smooth_tag.enabled', $enableSmoothTag);
+                $pkg->getConfig()->save('archebian.smooth_tag.exclude', $excludeSelectors);
+                $pkg->getConfig()->save('archebian.smooth_tag.include', $includeSelectors);
                 $this->redirect('/dashboard/smooth_tag/settings','updated');
             }
         } else {
